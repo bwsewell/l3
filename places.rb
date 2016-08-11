b85e3c50effef7360c8637bd0297eb3c39659be8
Brian Sewell <bwsewell@gmail.com>
2016-08-11 17:14:46 -0400

    Stub out modules for calculations

---
+++ places.rb
@@ -0,0 +1,25 @@
+module Places
+  def self.grocery
+
+  end
+
+  def self.hospital
+
+  end
+
+  def self.shopping
+
+  end
+
+  def self.universities
+
+  end
+
+  def self.restaurants
+
+  end
+
+  def self.restaurants
+
+  end
+end
---
+++ weather.rb
@@ -0,0 +1,21 @@
+module Weather
+  def self.rainfall
+
+  end
+
+  def self.snowfall
+
+  end
+
+  def self.avg_temperature
+
+  end
+
+  def self.high_temperature
+
+  end
+
+  def self.low_temperature
+
+  end
+end
