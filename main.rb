bd0fc906f4a91d205b9827825337d16ce13904e5
Brian Sewell <bwsewell@gmail.com>
2016-08-11 17:24:34 -0400

    We have a view

--- main.rb
+++ main.rb
@@ -1,9 +1,24 @@
+require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'net/http'
+require 'net/https'
+require 'uri'
 require 'json'
+require 'time'
+
+DIRECTIONS_URI =  URI("https://maps.googleapis.com/maps/api/directions/json")
+PLACES_URI =      URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
+GEOCODE_URI =     URI("https://maps.googleapis.com/maps/api/geocode/json")
+API_KEY =         "AIzaSyA4aFp7KfGDr9g6MEems8GjxtRqufkZBFE"
 
 get '/' do
+  haml :index
+end
+
+post '/' do
   content_type :json
-  "Hello World"
+  address = params[:address]
+  household = params[:household]
+  places = params[:places]
 end
